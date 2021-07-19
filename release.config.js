95655dfdf1880bf10c99c602cb594c3cd4c57aeb
sobolevn <mail@sobolevn.me>
2021-07-19 20:54:33 +0300

    Adds @semantic-release/exec to the pipeline

--- release.config.js
+++ release.config.js
@@ -1,24 +1,21 @@
 // Generic release configuration.
 //
-// Parameters are:
+// Required parameters are:
 // 1. KIRA_RELEASE_ASSETS
 //    to release static files with gitlab
-// 2. KIRA_RELEASE_SKIP_DOCKER=true
-//    to skip docker release to Gitlab Registry
-// 2.1 GROUP_NAME and PROJECT_NAME
+// 2. GROUP_NAME and PROJECT_NAME
 //    to correctly set name of your docker image
 //    if you are using docker releases
-// 3. KIRA_RELEASE_REPLACE_CONFIG
-//    json config of how to replace version number in the project file
 //
+// See README.md for more options and their description.
 
 // Configuration:
 const assets = process.env.KIRA_RELEASE_ASSETS || ''
 const replaceConfig = JSON.parse(
   process.env.KIRA_RELEASE_REPLACE_CONFIG || '{}',
 )
+const execConfig = JSON.parse(process.env.KIRA_RELEASE_EXEC_CONFIG || '{}')
 const skipDocker = process.env.KIRA_RELEASE_SKIP_DOCKER
-const dockerImageName = `${process.env.GROUP_NAME}/${process.env.PROJECT_NAME}`
 
 // Files to be committed back to the repo later on:
 const tobeCommitted = ['CHANGELOG.md']
@@ -59,6 +56,17 @@ if (replaceConfig) {
   ])
 }
 
+// Maybe we should execute some extra steps before making a deployment?
+if (Object.keys(execConfig).length !== 0) {
+  console.log('Found exec configuration')
+  console.log(execConfig)
+
+  releasePipeline.plugins.push([
+    // See: https://github.com/semantic-release/exec
+    '@semantic-release/exec', execConfig,
+  ])
+}
+
 // Back to basic release pipeline:
 releasePipeline.plugins.push(...[
   ['@semantic-release/git', {
@@ -73,7 +81,7 @@ if (!skipDocker || skipDocker.toLowerCase() !== 'true') {
   // If it is not a docker-based app, this step will be ignored:
   releasePipeline.plugins.push([
     'semantic-release-gitlab-docker', {
-      'name': dockerImageName,
+      'name': `${process.env.GROUP_NAME}/${process.env.PROJECT_NAME}`,
     },
   ])
 }
