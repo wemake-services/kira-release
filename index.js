1f01cd3b8716537bf0908d3c77b0eca1259ea82d
sobolevn <mail@sobolevn.me>
2023-07-27 14:29:55 +0300

    Use internal gitlab-ci releases

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// This is a friendly fork of
+// https://gitlab.com/foxfarmroad/semantic-release-gitlab-docker
+
 const verifyConditions = require('./lib/verify')
 const publish = require('./lib/publish')
 
